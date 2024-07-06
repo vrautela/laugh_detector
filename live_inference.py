524d4c21d372e6d023e129944dfa032d9e8d978b
Vedant Rautela <v.rautela01@gmail.com>
2024-07-05 20:41:55 -0400

    first commit: add data prep script, model training script, and lots of data files and checkpoints

---
+++ live_inference.py
@@ -0,0 +1,13 @@
+"""A module/script that continuously records audio and periodically determines whether laughter occurred during chunks of the recording"""
+
+def main():
+    # load laugh detector into local variable
+    # while loop
+        # record 2 seconds of audio
+        # extract features using vggish_inference_demo
+        # turn features into a pandas DataFrame
+        # call laugh detector on features
+        # print whether or not laughter was detected
+
+if __name__ == '__main__':
+    main()
